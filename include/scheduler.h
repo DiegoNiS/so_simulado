aea79c3ffe6ffb7cfde145d382334d3e9e028654
Diego Nina <dninas@unsa.edu.pe>
2024-12-14 23:28:00 -0500

    planificador del tiempo de CPU a los procesos con 2 algoritmos

--- include/scheduler.h
+++ include/scheduler.h
@@ -3,7 +3,9 @@
 
 #include "process.h"
 
+// Algoritmo de planificación First Come First Serve
 void fcfs_schedule(Process *processes, int n);
+// Algoritmo de planificación Round Robin
 void round_robin_schedule(Process *processes, int n, int quantum);
 
 #endif
