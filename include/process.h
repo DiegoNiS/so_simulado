d4a85b8fc14e852ce88769ab6a46ff4c82cbb866
Diego Nina <dninas@unsa.edu.pe>
2024-12-14 23:16:43 -0500

    Definicion corta y simple de los procesos

---
+++ include/process.h
@@ -0,0 +1,19 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+typedef enum { NEW, READY, RUNNING, WAITING, TERMINATED } ProcessState;
+
+typedef struct {
+    int pid;                // Identificador unique del proceso
+    char name[20];          // Nombre del proceso
+    ProcessState state;     // Estado actual del proceso
+    int priority;           // Prioridad del proceso
+    int burst_time;         // Tiempo de ráfaga del proceso
+    int arrival_time;       // Tiempo en que llega el proceso
+} Process;
+
+// Funciones
+void create_process(Process *process, int pid, char *name, int burst_time, int arrival_time, int priority);
+void display_process(const Process *p);
+
+#endif
